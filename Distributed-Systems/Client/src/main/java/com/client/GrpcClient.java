e5ff58d7888c814afc44c9b4afabaebddbf1283f
SammarTahir <G00347526@gmit.ie>
2019-11-28 21:17:01 +0000

    sample

--- Distributed-Systems/Client/src/main/java/com/client/GrpcClient.java
+++ Distributed-Systems/Client/src/main/java/com/client/GrpcClient.java
@@ -1,16 +1,21 @@
 package com.client;
 
+
 import com.grpcproject.User.APIResponse;
 import com.grpcproject.User.LoginRequest;
 import com.grpcproject.userGrpc;
 import com.grpcproject.userGrpc.userBlockingStub;
+import com.user.UserApiApplication;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import com.*;
 
 public class GrpcClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		
+		new UserApiApplication().run(args);
 		
 		String userID = "G00347526";
 		String password = "G00347526";
